ee0735482112e9ec5d60c6089b9b7bbd8012bb7f
Oscar Bejarano <oscwne@gmail.com>
2019-09-26 09:23:30 -0500

    make public base repository interface

--- Salesgram.Core/BaseRepository/IBaseRepository.cs
+++ Salesgram.Core/BaseRepository/IBaseRepository.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace Salesgram.Core.BaseRepository
 {
-    interface IBaseRepository<TEntity> where TEntity : class
+    public interface IBaseRepository<TEntity> where TEntity : class
     {
         Task<IEnumerable<TEntity>> GetAsync(Expression<Func<TEntity, bool>> filter = null, Expression<Func<TEntity, object>>[] navigationProperties = null);
         Task<TEntity> GetByIdAsync(object id);
